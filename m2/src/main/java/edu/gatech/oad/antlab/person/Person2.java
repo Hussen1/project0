e519c783e3352f850aaf677e4120d9446706f079
Hassen1 <hassui2012@gmail.com>
2016-03-03 20:30:25 -0500

    added new file for trail

---
+++ m2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -0,0 +1,53 @@
+package edu.gatech.oad.antlab.person;
+import java.util.Collections;
+import java.util.Arrays;
+/**
+ *  A simple class for person 2
+ *  returns their name and a
+ *  modified string 
+ *
+ * @author Bob
+ * @version 1.1
+ */
+public class Person2 {
+    /** Holds the persons real name */
+    private String name;
+	 	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+	 public Person2(String pname) {
+	   name = pname;
+	 }
+	/**
+	 * This method should take the string
+	 * input and return its characters in
+	 * random order.
+	 * given "gtg123b" it should return
+	 * something like "g3tb1g2".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+		String[] newArray = input.split("");
+		String value = "";
+		Collections.shuffle(Arrays.asList(newArray));
+		for(int i = 0; i < newArray.length; i++) {
+			value = value +  newArray[i];
+		}
+		return value;
+	}
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+}
